e2796afdcb1493f85cb56af1c2e802b560953d3b
Pavel Cherdantsev <ch3pash@yandex.ru>
2022-10-03 22:00:08 +0400

    feat: add smooth animation for fomrs appear and disappear

--- scripts/index.js
+++ scripts/index.js
@@ -17,6 +17,73 @@ const elementsContainer = document.querySelector(".elements__list");
 const template = document.querySelector(".template");
 
 
+//функция добавления like на карточке
+const handleLikeCard = (e) => {
+  const currentLikeBtn = e.target.closest('.button_type_like');
+  currentLikeBtn.classList.toggle('button_active');
+}
+
+//функция закрытия формы редактирования профиля с сохранением
+const handleSaveEdit = function (e) {
+  e.preventDefault();
+  currentPopup.classList.remove('popup_opened');
+  profileName.textContent = PopupInputName.value;
+  profileOccupation.textContent = PopupInputOccupation.value;
+}
+
+//универсальная функция открытия форм
+const handleOpenPopup = (e) => {
+
+  //если кликнули по кнопке edit редактирования профиля
+  if (e.target.classList.contains('button_type_edit')) {
+    currentPopup = document.querySelector('.popup_type_profile-edit');
+    PopupInputName.value = profileName.textContent;
+    PopupInputOccupation.value = profileOccupation.textContent;
+
+    currentPopup.addEventListener('submit', handleSaveEdit);
+  }
+
+  //если кликнули по кнопке add добавления карточки
+  if (e.target.classList.contains('button_type_add')) {
+    currentPopup = document.querySelector('.popup_type_add-card');
+    PopupInputPlace.value = '';
+    PopupInputImgLink.value = '';
+
+    currentPopup.addEventListener('submit', handleAddCard);
+  }
+
+  //если кликнули по картинке карточки
+  if (e.target.classList.contains('element__image')) {
+    currentPopup = document.querySelector('.popup_type_card-view');
+    currentPopup.classList.add('popup_opened');
+
+    const currentPopupImgEl = currentPopup.querySelector('.popup__image');
+    currentPopupImgEl.src = e.target.src;
+    currentPopupImgEl.alt = e.target.alt;
+
+    const currentCard = e.target.closest('.element'); //ищем ближайшего родителя, дом элемент всей карточки, по которой кликнули
+    const currentCardCaption = currentCard.querySelector('.element__caption'); //теперь в полученной карточке ищем подпись к картинке, хотя можно было взять из e.target.alt;
+    const currentPopupCaption = currentPopup.querySelector('.popup__image-caption');
+    currentPopupCaption.textContent = currentCardCaption.textContent;
+  }
+
+  currentPopup.classList.add('popup_opened');
+  const closeButton = currentPopup.querySelector('.popup__close');
+  closeButton.addEventListener('click', handleСlosePopup);
+}
+
+//функция закрытия формы без сохранения
+const handleСlosePopup = () => {
+  currentPopup.classList.add('popup_closed'); //плавно закрываемся с помощью класса стиля
+
+  // 0.3 секунды ждём пока закончится плавная анимация скрытия формы и чистим больше ненужные классы
+  setTimeout(function(){
+    currentPopup.classList.remove('popup_opened');
+    currentPopup.classList.remove('popup_closed');
+    currentPopup = null;
+  }, 300);
+}
+
 //функция формирования DOM элемента из template
 const createElementNode = (name, link) => {
   const currentElement = template.content.cloneNode(true);
@@ -54,22 +121,7 @@ const handleDeleteCard = (e) => {
   currentCard.remove();
 }
 
-//функция добавления like на карточке
-const handleLikeCard = (e) => {
-  const currentLikeBtn = e.target.closest('.button_type_like');
-  currentLikeBtn.classList.toggle('button_active');
-}
-
-
-//функция закрытия формы редактирования профиля с сохранением
-const handleSaveEdit = function (e) {
-  e.preventDefault();
-  currentPopup.classList.remove('popup_opened');
-  profileName.textContent = PopupInputName.value;
-  profileOccupation.textContent = PopupInputOccupation.value;
-}
-
-//функция закрытия формы с сохранением
+//функция закрытия формы добавления карточки с сохранением
 const handleAddCard = function (e) {
   e.preventDefault();
   currentPopup.classList.remove('popup_opened');
@@ -77,63 +129,8 @@ const handleAddCard = function (e) {
   render();
 }
 
-
-//универсальная функция открытия форм
-const handleOpenPopup = (e) => {
-
-
-  //если кликнули по кнопке edit редактирования профиля
-  if (e.target.classList.contains('button_type_edit')) {
-    currentPopup = document.querySelector('.popup_type_profile-edit');
-    PopupInputName.value = profileName.textContent;
-    PopupInputOccupation.value = profileOccupation.textContent;
-
-    currentPopup.addEventListener('submit', handleSaveEdit);
-  }
-
-  //если кликнули по кнопке add добавления карточки
-  if (e.target.classList.contains('button_type_add')) {
-    currentPopup = document.querySelector('.popup_type_add-card');
-    PopupInputPlace.value = 'Архыз';
-    PopupInputImgLink.value = './images/place_2016arhys.jpg';
-
-    currentPopup.addEventListener('submit', handleAddCard);
-  }
-
-  //если кликнули по картинке карточки
-  if (e.target.classList.contains('element__image')) {
-    currentPopup = document.querySelector('.popup_type_card-view');
-    currentPopup.classList.add('popup_opened');
-
-    const currentPopupImgEl = currentPopup.querySelector('.popup__image');
-    currentPopupImgEl.src = e.target.src;
-    currentPopupImgEl.alt = e.target.alt;
-
-    const currentCard = e.target.closest('.element'); //ищем ближайшего родителя, дом элемент всей карточки, по которой кликнули
-    const currentCardCaption = currentCard.querySelector('.element__caption'); //теперь в полученной карточке ищем подпись к картинке, хотя можно было взять из e.target.alt;
-    const currentPopupCaption = currentPopup.querySelector('.popup__image-caption');
-    currentPopupCaption.textContent = currentCardCaption.textContent;
-  }
-
-
-  currentPopup.classList.add('popup_opened');
-  const closeButton = currentPopup.querySelector('.popup__close');
-  closeButton.addEventListener('click', handleСlosePopup);
-}
-
-
-
-//функция закрытия формы без сохранения
-const handleСlosePopup = function () {
-  currentPopup.classList.remove('popup_opened');
-  currentPopup = null;
-}
-
 newCardBtn.addEventListener('click', handleOpenPopup);
 editProfileBtn.addEventListener('click', handleOpenPopup);
-
-
-
 
 //вызываем функцию начальной отрисовки карточек
 render();
